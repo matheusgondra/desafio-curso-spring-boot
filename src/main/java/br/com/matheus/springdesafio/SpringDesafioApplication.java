8aee4379ce12162eae108c4638c76866c06c2d4f
Matheus de Gondra <matheusgondra951@gmail.com>
2023-12-18 09:23:09 -0300

    commit initial

---
+++ src/main/java/br/com/matheus/springdesafio/SpringDesafioApplication.java
@@ -0,0 +1,12 @@
+package br.com.matheus.springdesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDesafioApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/matheus/springdesafio/SpringDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.springdesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
